23e4f3d3792d1b153e8773c4d4f4d6addae90419
mariaalexus <mariaalexusramos@gmail.com>
2022-03-01 17:18:17 -0800

    final project

--- index.js
+++ index.js
@@ -2,90 +2,83 @@
 var countDownTimer;
 var timerInterval;
 var arrayOfQuestions = [];
-var gameScore = 0; //initializes the game score to zero 
-var timePenalty = 30; //thirty seconds
+var gameScore = 0; //initializes the game score to zero
+const timePenalty = 30; //time in seconds; thirty seconds
 const maxTime = 5 * 60; //time is in seconds
 function setCountDownTimer() {
     countDownTimer = maxTime;
     console.log("countDownTimer: ", countDownTimer);
 }
 
-//when the person clicks begin button the timer starts
-//start timer
-function startTimer () {
-    timerInterval = setInterval(updateTimer, 1000); //every second, aka every one thousand milliseconds
+//When the person clicks the begin button the timer starts
+//start time
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
     console.log("startTimer: ", timerInterval);
 }
 
 function updateTimer() {
-    console.log("updateTimer: begin");
-    countDownTimer--; //counts down the timer by one
+    console.log("updateTimer: begun");
+    countDownTimer--;
     console.log("updateTimer: countDownTimer", countDownTimer);
     presentTimer();
 }
 
-function presentTimer () {
+function presentTimer() {
     let currentTime = document.getElementById('timer');
     currentTime.innerText = countDownTimer;
-    console.log("showTimer: currentTime", currentTime);
+    console.log("presentTimer: currentTime", currentTime);
 }
 
-function showScoreBoard () {
+function showScoreBoard() {
     let scoreBoard = document.getElementById('score-board');
     console.log("showScoreBoard: scoreBoard", scoreBoard);
-   // scoreBoard.classList.toggle('hide', false);
+    //scoreBoard.classList.toggle('hide', false);
     scoreBoard.classList.remove('hide');
 }
 
-function presentScore () {
+function presentScore() {
     let currentScore = document.getElementById('score');
-    currentScore.innertext = gameScore;
+    currentScore.innerText = gameScore;
     console.log("presentScore: currentScore", currentScore);
 }
 
 function hideGreeting() {
-    let Greeting = document.getElementById('greeting');
-    console.log("hideGreeting: greeting", Greeting);
-   // greeting.classList.toggle('hide', false);
+    let greeting = document.getElementById('greeting');
+    console.log("hideGreeting: greeting", greeting);
+    //greeting.classList.toggle('hide', false);
     greeting.classList.add('hide');
 }
 
-function doGame() {
-    setCountDownTimer();
-    startTimer ();
-    hideGreeting ();
-    showScoreBoard ();
-    presentScore ();
-    getQuestion ('quest0');
-}
 
-//question is asked
-function SetupQuestionsAndAnswers() {
-    console.log("SetUpQuestions started")
+
+//questions is asked
+function  setupQuestionsAndAnswers() {
+    console.log("SetupQuestions started");
     let question1 = {
         id: "quest0",
-        question: "To select elements with a specific class:",
-        answer1: "write a semicolon (;) character, followed by fdasfsad",
-        answer2: "write a semicolon (;) character, followed by fjsdlkjfkljfkjlkjlkjkljkljkl" ,
-        answer3: "write a period (.) character",
+        question: "To select elements with a specific class: ",
+        answer1: " write a semicolon (;) character, followed by the class name.",
+        answer2: " write a period (.) character, followed by the class name.",
+        answer3: " write a period (.) character",
         answer4: "write a comma (,) character",
-        correctAnswer:2
+        correctAnswer: 2
     };
 
     let question2 = {
         id: "quest1",
-        question: "In CSS, a color can be specified",
+        question: "In CSS, a color can be specified by using a predefined color name.",
         answer1: "yes",
         answer2: "no",
         answer3: null,
         answer4: null,
-        correctAnswer:1
+        correctAnswer: 1
     };
 
     arrayOfQuestions.push(question1);
     arrayOfQuestions.push(question2);
 
-    console.log("SetUpQuestions arrayOfQuestions: ", arrayOfQuestions)
+    console.log("SetupQuestions arrayOfQuestions: ", arrayOfQuestions);
 
 }
 
@@ -93,29 +86,27 @@ function getQuestion(questionId) {
     console.log('getQuestion questionId', questionId);
 
     let found = false;
-    
+
     for(let i = 0; i<arrayOfQuestions.length; i++) {
         if(questionId == arrayOfQuestions[i].id) {
-            prepareQuestionandAnswers(i); 
+            prepareQuestionAndAnswers(i);
         }
     }
 }
 
-function prepareQuestionandAnswers(questionIndex) {
+function prepareQuestionAndAnswers(questionIndex) {
     console.log('prepareQuestionAndAnswers questionIndex', questionIndex);
-    let questionSection = document.getElementId('questions');
-    let newQuestion= document.createElement('div');
-    newQuestion.id = arrayOfQuestions[questionIndex].id
-    newQuestion.innerText = arrayOfQuestions[questionIndex].question;
+    let questionSection = document.getElementById('questions');
+    let newQuestion = document.createElement('div');
+    newQuestion.id = arrayOfQuestions[questionIndex].id;
     newQuestion.classList.add('question');
-    newQuestion.appendChild(newQuestion);
-
+    
     let qSpan = document.createElement('span');
-    qSpan.innertext = arrayOfQuestions[questionIndex].question;
+    qSpan.innerText = arrayOfQuestions[questionIndex].question;
     newQuestion.appendChild(qSpan);
 
 
-    if(arrayOfQuestions [questionIndex].answer1) {
+    if(arrayOfQuestions[questionIndex].answer1) {
         let answer = document.createElement('div');
         let rdoBtn = document.createElement('input');
         let label = document.createElement('label');
@@ -124,42 +115,47 @@ function prepareQuestionandAnswers(questionIndex) {
         rdoBtn.setAttribute('type', 'radio');
         rdoBtn.value = 1
         rdoBtn.classList.add('answer');
-        rdoBtn.classList.add('answer') = arrayOfQuestions[questionIndex].id;
+        rdoBtn.classList.add(`${arrayOfQuestions[questionIndex].id}`);
         rdoBtn.name = arrayOfQuestions[questionIndex].id;
 
-        label.innertext = arrayOfQuestions[questionIndex].answer1;
+        label.innerText = arrayOfQuestions[questionIndex].answer1;
         label.setAttribute('for', `${rdoBtn.id}`);
 
         answer.classList.add('answer');
         answer.appendChild(rdoBtn);
         answer.appendChild(label);
-        newQuestion.appendChild('answer');
+        newQuestion.appendChild(answer);
 
         console.log('prepareQuestionAndAnswers answer', answer);
-
-
-        answer.innertext = arrayOfQuestions[questionIndex].answer1;
-        answer.classList.add('answer');
-        answer.id = "" + arrayOfQuestions[questionIndex].id + "_answer1";
-        //answer.id = `${arrayOfQuestions[questionIndex].id}_answer1`;
-        newQuestion.appendChild(answer);
     }
 
-    if(arrayOfQuestions [questionIndex].answer2) {
+    if(arrayOfQuestions[questionIndex].answer2) {
         let answer = document.createElement('div');
-        answer.innertext = arrayOfQuestions[questionIndex].answer1;
+        answer.innerText = arrayOfQuestions[questionIndex].answer2;
         answer.classList.add('answer');
         answer.id = `${arrayOfQuestions[questionIndex].id}_answer2`;
         newQuestion.appendChild(answer);
 
+        console.log('prepareQuestionAndAnswers answer', answer);
     }
 
 
+    questionSection.appendChild(newQuestion);
 
+    console.log('prepareQuestionAndAnswers newQuestion', newQuestion);
 }
 
 //if answered correctly, show points and go to next question
-
 //if answered incorrectly, time penalty by reducing time and go to next question
 //once all question have been answered, give options
 //if user chooses to save score show the scoreboard
+
+function doGame() {
+    setCountDownTimer();
+    startTimer();
+    hideGreeting();
+    setupQuestionsAndAnswers();
+    showScoreBoard();
+    presentScore();
+    getQuestion('quest0');
+}
